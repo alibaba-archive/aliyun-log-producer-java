b95e00d9efba7a7b6d7f18c2e7b4914c944d7ce2
brucewu-fly <brucewu.fly.cn@gmail.com>
2018-09-01 10:16:17 +0800

    Change clientpool logic. Per project per client

--- src/main/java/com/aliyun/openservices/log/producer/inner/ClientPool.java
+++ src/main/java/com/aliyun/openservices/log/producer/inner/ClientPool.java
@@ -1,28 +1,15 @@
 package com.aliyun.openservices.log.producer.inner;
 
-import java.util.HashMap;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.aliyun.openservices.log.Client;
 import com.aliyun.openservices.log.producer.ProducerConfig;
 import com.aliyun.openservices.log.producer.ProjectConfig;
 
 public class ClientPool {
-    class Pair {
-        public Client client;
-        public int ref;
 
-        public Pair(Client client, int ref) {
-            super();
-            this.client = client;
-            this.ref = ref;
-        }
-    }
-
-    private HashMap<String, String> projectEndpointMap = new HashMap<String, String>();
-    private HashMap<String, Pair> clientPool = new HashMap<String, Pair>();
-    private ReadWriteLock rwLock = new ReentrantReadWriteLock();
+    private Map<String, Client> clientPool = new ConcurrentHashMap<String, Client>();
     private ProducerConfig producerConfig;
 
     public ClientPool(ProducerConfig producerConfig) {
@@ -30,58 +17,17 @@ public class ClientPool {
     }
 
     public Client updateClient(final ProjectConfig config) {
-        rwLock.writeLock().lock();
-        try {
-            String ep = projectEndpointMap.get(config.projectName);
-            int ref = 0;
-            if (ep == null) {
-                ref = 1;
-                projectEndpointMap.put(config.projectName, config.endpoint);
-                ep = config.endpoint;
-            }
-            Pair p = clientPool.get(ep);
-            Client client;
-            if (p != null) {
-                client = p.client;
-                p.ref += ref;
-            } else {
-                client = buildClient(config);
-                clientPool.put(ep, new Pair(client, ref));
-            }
-            return client;
-        } finally {
-            rwLock.writeLock().unlock();
-        }
+        Client client = buildClient(config);
+        clientPool.put(config.projectName, client);
+        return client;
     }
 
     public void removeClient(final String project) {
-        rwLock.writeLock().lock();
-        try {
-            String ep = projectEndpointMap.get(project);
-            if (ep != null) {
-                projectEndpointMap.remove(project);
-                Pair p = clientPool.get(ep);
-                if (--p.ref == 0) {
-                    clientPool.remove(ep);
-                }
-            }
-        } finally {
-            rwLock.writeLock().unlock();
-        }
+        clientPool.remove(project);
     }
 
     public Client getClient(final String project) {
-        rwLock.readLock().lock();
-        try {
-            String ep = projectEndpointMap.get(project);
-            Client c = null;
-            if (ep != null) {
-                c = clientPool.get(ep).client;
-            }
-            return c;
-        } finally {
-            rwLock.readLock().unlock();
-        }
+        return clientPool.get(project);
     }
 
     private Client buildClient(final ProjectConfig config) {
